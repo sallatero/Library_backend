6fba3f9cfd3c420901d03f7df61d419f9bf08985
Salla <salla.tero@gmail.com>
2019-06-17 12:57:35 +0300

    uusi build

--- build/precache-manifest.f30f7722407fd3c62c2edf2afcc5334a.js
+++ build/precache-manifest.f30f7722407fd3c62c2edf2afcc5334a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "7fdb9822b96193dddb02",
-    "url": "/static/js/main.7fdb9822.chunk.js"
+    "revision": "10aaaf4e11c434570efc",
+    "url": "/static/js/main.10aaaf4e.chunk.js"
   },
   {
     "revision": "34982dfab0f5e4f58618",
     "url": "/static/js/2.34982dfa.chunk.js"
   },
   {
-    "revision": "7fdb9822b96193dddb02",
+    "revision": "10aaaf4e11c434570efc",
     "url": "/static/css/main.822693a4.chunk.css"
   },
   {
-    "revision": "835fd9873f55b88c18f072c949d11868",
+    "revision": "a1edf7c147fad687aa6c4f1ebe3f09fe",
     "url": "/index.html"
   }
 ];
